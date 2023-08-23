d6b5cf3a0f2f16ba0b77195ec48ed8287866030c
Socheat Sok <socheatsok78@gmail.com>
2023-08-23 13:28:37 +0700

    Implement attach, detach & detachAll methods

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
+type RegisteredHandler<K extends keyof AbortSignalEventMap> = (this: AbortSignal, ev: AbortSignalEventMap[K]) => any
+
 export class AggregatedAbortController extends AbortController {
-  private signals: WeakMap<AbortSignal, boolean> = new WeakMap<AbortSignal, boolean>()
+  private signals: Set<AbortSignal> = new Set()
+  private registers: WeakMap<AbortSignal, RegisteredHandler<any>> = new WeakMap()
 
   constructor(items: (AbortController | AbortSignal)[]) {
     super()
+
     for (const item of items) {
       const signal: AbortSignal | undefined = item instanceof AbortController
         ? item.signal
@@ -10,13 +14,47 @@ export class AggregatedAbortController extends AbortController {
           ? item
           : undefined
 
-      if (signal !== undefined) this.link(signal)
+      if (signal !== undefined) this.attach(signal)
     }
   }
 
-  public link(signal: AbortSignal): void {
-    if (this.signals.has(signal)) return;
-    this.signals.set(signal, true)
-    signal.addEventListener('abort', () => this.abort())
+  /**
+   * Attach a signal to this controller.
+   */
+  public attach(signal: AbortSignal): boolean {
+    if (this.registers.has(signal)) {
+      return false
+    }
+
+    const handler = () => this.abort()
+    this.signals.add(signal)
+    this.registers.set(signal, handler)
+    signal.addEventListener('abort', handler)
+    return true
+  }
+
+  /**
+   * Detach a signal from this controller.
+   */
+  public detach(signal: AbortSignal): boolean {
+    const handler = this.registers.get(signal)
+    
+    if (handler === undefined) {
+      return false
+    }
+
+    signal.removeEventListener('abort', handler)
+    this.signals.delete(signal)
+    this.registers.delete(signal)
+    return true
+  }
+
+  /**
+   * Detach all signals from this controller.
+   */
+  public detachAll(): void {
+    for (const signal of this.signals) {
+      this.detach(signal)
+    }
   }
 }
